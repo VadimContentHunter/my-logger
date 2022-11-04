af43810b61b4e2748ba43f41e386b3fae92c6e49
Vadim <project.k.vadim@gmail.com>
2022-11-04 06:47:44 +0200

    added 4 tests

--- tests/formattersTests/BaseFormatterTest.php
+++ tests/formattersTests/BaseFormatterTest.php
@@ -16,9 +16,59 @@ class BaseFormatterTest extends TestCase
         $this->baseFormatter = new BaseFormatter();
     }
 
-    public function testOne()
+    /**
+     * Тест аргументов метода, в виде обычного сообщения и без параметров для контекста.
+     *
+     * @return void
+     */
+    public function testNormalMessageWithoutParameterContexts()
     {
-        $resultMessage = $this->baseFormatter->getMessage("my test", []);
-        $this->assertEquals('', $resultMessage);
+        $baseMessage = 'The file was created successfully.';
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage);
+        $this->assertEquals($baseMessage, $resultMessage);
+    }
+
+    /**
+     * Тест аргументов метода, в виде сообщения с заполнителями и без параметров для контекста.
+     *
+     * @return void
+     */
+    public function testMessageWithPlaceholdersWithoutParameterContexts()
+    {
+        $baseMessage = 'The file [{name}] was created successfully.';
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage);
+        $this->assertEquals($baseMessage, $resultMessage);
+    }
+
+    /**
+     * Тест аргументов метода, в виде обычного сообщения и с параметрами для контекста.
+     *
+     * @return void
+     */
+    public function testNormalMessageWithParameterContexts()
+    {
+        $baseMessage = 'The file was created successfully.';
+        $baseParameters =   [
+                                ['file_name' => 'new_file.txt'],
+                                ['action' => 'created']
+                            ];
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
+        $this->assertEquals($baseMessage, $resultMessage);
+    }
+
+    /**
+     * Тест аргументов метода, в виде сообщения с заполнителями и с параметрами для контекста.
+     *
+     * @return void
+     */
+    public function testMessageWithPlaceholdersWithParameterContexts()
+    {
+        $baseMessage = 'The file [{file_name}] was {action} successfully.';
+        $baseParameters =   [
+                                ['file_name' => 'new_file.txt'],
+                                ['action' => 'created']
+                            ];
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
+        $this->assertEquals($baseMessage, $resultMessage);
     }
 }
