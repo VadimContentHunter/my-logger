51915b48d0ab000aa66c79aca1389ca8dce8784a
Vadim <project.k.vadim@gmail.com>
2022-11-05 06:36:15 +0200

    Added a class for common formatting tests

--- tests/formattersTests/BaseFormatterTest.php
+++ tests/formattersTests/BaseFormatterTest.php
@@ -4,71 +4,13 @@ declare(strict_types=1);
 
 namespace vadimcontenthunter\MyLogger\Tests\formattersTests;
 
-use PHPUnit\Framework\TestCase;
 use vadimcontenthunter\MyLogger\formatters\BaseFormatter;
+use vadimcontenthunter\MyLogger\Tests\src\BaseTestsFormatters;
 
-class BaseFormatterTest extends TestCase
+class BaseFormatterTest extends BaseTestsFormatters
 {
-    protected BaseFormatter $baseFormatter;
-
     public function setUp(): void
     {
         $this->baseFormatter = new BaseFormatter();
-    }
-
-    /**
-     * Тест аргументов метода, в виде обычного сообщения и без параметров для контекста.
-     *
-     * @return void
-     */
-    public function testNormalMessageWithoutParameterContexts()
-    {
-        $baseMessage = 'The file was created successfully.';
-        $resultMessage = $this->baseFormatter->getMessage($baseMessage);
-        $this->assertEquals($baseMessage, $resultMessage);
-    }
-
-    /**
-     * Тест аргументов метода, в виде сообщения с заполнителями и без параметров для контекста.
-     *
-     * @return void
-     */
-    public function testMessageWithPlaceholdersWithoutParameterContexts()
-    {
-        $baseMessage = 'The file [{name}] was created successfully.';
-        $resultMessage = $this->baseFormatter->getMessage($baseMessage);
-        $this->assertEquals($baseMessage, $resultMessage);
-    }
-
-    /**
-     * Тест аргументов метода, в виде обычного сообщения и с параметрами для контекста.
-     *
-     * @return void
-     */
-    public function testNormalMessageWithParameterContexts()
-    {
-        $baseMessage = 'The file was created successfully.';
-        $baseParameters =   [
-                                ['file_name' => 'new_file.txt'],
-                                ['action' => 'created']
-                            ];
-        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
-        $this->assertEquals($baseMessage, $resultMessage);
-    }
-
-    /**
-     * Тест аргументов метода, в виде сообщения с заполнителями и с параметрами для контекста.
-     *
-     * @return void
-     */
-    public function testMessageWithPlaceholdersWithParameterContexts()
-    {
-        $baseMessage = 'The file [{file_name}] was {action} successfully.';
-        $baseParameters =   [
-                                ['file_name' => 'new_file.txt'],
-                                ['action' => 'created']
-                            ];
-        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
-        $this->assertEquals($baseMessage, $resultMessage);
     }
 }
---
+++ tests/src/BaseTestsFormatters.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyLogger\Tests\src;
+
+use PHPUnit\Framework\TestCase;
+use vadimcontenthunter\MyLogger\interfaces\Formatter;
+
+class BaseTestsFormatters extends TestCase
+{
+    protected ?Formatter $baseFormatter = null;
+
+    /**
+     * Тест метода, с аргументами в виде обычного сообщения и без параметров для контекста.
+     *
+     * @return void
+     */
+    public function testMessageWithoutPlaceholdersAndParameterContexts()
+    {
+        $expectedResult = 'The file was created successfully.';
+        $baseMessage = 'The file was created successfully.';
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage);
+        $this->assertEquals($expectedResult, $resultMessage);
+    }
+
+    /**
+     * Тест метода, с аргументами в виде сообщения с заполнителями и без параметров для контекста.
+     *
+     * @return void
+     */
+    public function testMessageWithPlaceholdersWithoutParameterContexts()
+    {
+        $expectedResult = 'The file {name} was created successfully.';
+        $baseMessage = 'The file {name} was created successfully.';
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage);
+        $this->assertEquals($expectedResult, $resultMessage);
+    }
+
+    /**
+     * Тест метода, с аргументами в виде обычного сообщения и с параметрами для контекста.
+     *
+     * @return void
+     */
+    public function testMessageWithoutPlaceholdersWithParameterContexts()
+    {
+        $expectedResult = 'The file was created successfully.';
+        $baseMessage = 'The file was created successfully.';
+        $baseParameters =   [
+                                ['file_name' => 'new_file.txt'],
+                                ['action' => 'created']
+                            ];
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
+        $this->assertEquals($expectedResult, $resultMessage);
+    }
+
+    /**
+     * Тест метода, с аргументами в виде сообщения с заполнителями и с параметрами для контекста.
+     *
+     * @return void
+     */
+    public function testMessageWithPlaceholdersAndParameterContexts()
+    {
+        $expectedResult = 'The file new_file.txt was created successfully.';
+        $baseMessage = 'The file {file_name} was {action} successfully.';
+        $baseParameters =   [
+                                ['file_name' => 'new_file.txt'],
+                                ['action' => 'created']
+                            ];
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
+        $this->assertEquals($expectedResult, $resultMessage);
+    }
+
+    /**
+     * Тест метода, с аргументами в виде сообщения с заполнителями,которых БОЛЬШЕ чем параметров для контекста.
+     *
+     * @return void
+     */
+    public function testMessageContainsMorePlaceholdersThanParameterContexts()
+    {
+        $expectedResult = 'The file new_file.txt was created successfully.';
+        $baseMessage = 'The file {file_name} was {action} successfully.';
+        $baseParameters =   [
+                                ['file_name' => 'new_file.txt'],
+                            ];
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
+        $this->assertEquals($expectedResult, $resultMessage);
+    }
+
+    /**
+     * Тест метода, с аргументами в виде сообщения с заполнителями,которых МЕНЬШЕ чем параметров для контекста.
+     *
+     * @return void
+     */
+    public function testMessageContainsMoreParameterContextsThanPlaceholders()
+    {
+        $expectedResult = 'The file new_file.txt was created successfully.';
+        $baseMessage = 'The file {file_name} was {action} successfully.';
+        $baseParameters =   [
+                                ['file_name' => 'new_file.txt'],
+                                ['action' => 'created'],
+                                ['user' => 'user001'],
+                                ['date' => '05.11.2022'],
+                            ];
+        $resultMessage = $this->baseFormatter->getMessage($baseMessage, $baseParameters);
+        $this->assertEquals($expectedResult, $resultMessage);
+    }
+}
