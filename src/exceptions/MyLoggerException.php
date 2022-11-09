3403b765dc519767976fdf21497dab2539be3768
Vadim <project.k.vadim@gmail.com>
2022-11-09 03:08:46 +0200

    added phpdoc for files

--- src/exceptions/MyLoggerException.php
+++ src/exceptions/MyLoggerException.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace vadimcontenthunter\MyLogger\exceptions;
 
+/**
+ * Основной класс исключений для MyLogger
+ *
+ * @package   MyLogger_Exceptions
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
 class MyLoggerException extends \Exception
 {
-    public function __construct(string $_message = "")
+    /**
+     * Initializes the MyLoggerException
+     *
+     * @param string $_message Сообщение об исключении
+     */
+    public function __construct(string $_message = "Error MyLogger")
     {
         $this->message = $_message;
     }
