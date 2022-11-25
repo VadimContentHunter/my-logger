f0201b00fd8296752dab768b0e85a697ef2921a9
Vadim <project.k.vadim@gmail.com>
2022-11-26 00:27:25 +0200

    added new exception

---
+++ src/exceptions/IncorrectMessageGenerationException.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyLogger\exceptions;
+
+/**
+ * Исключение сообщает о том что класс не соответствует интерфейсу 'LoggerInterface'.
+ *
+ * @package   MyLogger_Exceptions
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class IncorrectMessageGenerationException extends MyLoggerException
+{
+    /**
+     * Initializes the IncorrectMessageGenerationException
+     */
+    public function __construct($_message = 'Incorrect message generation')
+    {
+        $this->message = $_message;
+    }
+}
