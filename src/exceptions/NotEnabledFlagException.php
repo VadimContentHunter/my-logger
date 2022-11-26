3e006b9ebec93339798732d9c7fd7a219e52379e
Vadim <project.k.vadim@gmail.com>
2022-11-26 05:24:42 +0200

    added new exception

---
+++ src/exceptions/NotEnabledFlagException.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyLogger\exceptions;
+
+/**
+ * Исключение сообщает о том что не активирован флаг.
+ *
+ * @package   MyLogger_Exceptions
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class NotEnabledFlagException extends MyLoggerException
+{
+    /**
+     * Initializes the SavingNotEnabledException
+     *
+     * @param mixed $_message
+     */
+    public function __construct($_message = 'Not enabled flag')
+    {
+        $this->message = $_message;
+    }
+}
