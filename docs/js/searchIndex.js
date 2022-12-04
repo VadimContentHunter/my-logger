497afeb0307da5e8006ad2b2c25f0cde76dfa79c
Vadim <project.k.vadim@gmail.com>
2022-12-04 04:10:02 +0200

    fixed: updated documentation

--- docs/js/searchIndex.js
+++ docs/js/searchIndex.js
@@ -18,7 +18,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\formatters\\BaseFormatter\u003A\u003A__construct\u0028\u0029",
             "name": "__construct",
-            "summary": "Initializes\u0020the\u0020MyLogger",
+            "summary": "Initializes\u0020the\u0020BaseFormatter",
             "url": "classes/vadimcontenthunter-MyLogger-formatters-BaseFormatter.html#method___construct"
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\formatters\\BaseFormatter\u003A\u003AsetMessageLog\u0028\u0029",
@@ -158,7 +158,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\MyLogger\u003A\u003Aexecute\u0028\u0029",
             "name": "execute",
-            "summary": "\u0412\u044B\u043F\u043E\u043B\u043D\u044F\u0435\u0442\u0020\u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044C\u0441\u043A\u0443\u044E\u0020\u0444\u0443\u043D\u043A\u0446\u0438\u044E\u0020\u043A\u043E\u0442\u043E\u0440\u0430\u044F\u0020\u0432\u0020\u043A\u0430\u0447\u0435\u0441\u0442\u0432\u0435\u0020\u0430\u0440\u0433\u0443\u043C\u0435\u043D\u0442\u0430\u0020\u043F\u0440\u0438\u043D\u0438\u043C\u0430\u0435\u0442\u0020\u0441\u043F\u0438\u0441\u043E\u043A\u0020\u0441\u0020\u043B\u043E\u0433\u0433\u0435\u0440\u0430\u043C\u0438.",
+            "summary": "\u0412\u044B\u043F\u043E\u043B\u043D\u044F\u0435\u0442\u0020\u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044C\u0441\u043A\u0443\u044E\u0020\u0444\u0443\u043D\u043A\u0446\u0438\u044E\u0020\u043A\u043E\u0442\u043E\u0440\u0430\u044F\u0020\u0432\u0020\u043A\u0430\u0447\u0435\u0441\u0442\u0432\u0435\u0020\u0430\u0440\u0433\u0443\u043C\u0435\u043D\u0442\u0430\u0020\u043F\u0440\u0438\u043D\u0438\u043C\u0430\u0435\u0442\u0020\u044D\u043B\u0435\u043C\u0435\u043D\u0442\u0020\u0438\u0437\u0020\u0441\u043F\u0438\u0441\u043A\u0430\u0020\u0441\u0020\u043B\u043E\u0433\u0433\u0435\u0440\u0430\u043C\u0438.",
             "url": "classes/vadimcontenthunter-MyLogger-MyLogger.html#method_execute"
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\MyLogger\u003A\u003Aemergency\u0028\u0029",
@@ -333,7 +333,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\IncorrectMessageGenerationException",
             "name": "IncorrectMessageGenerationException",
-            "summary": "\u0418\u0441\u043A\u043B\u044E\u0447\u0435\u043D\u0438\u0435\u0020\u0441\u043E\u043E\u0431\u0449\u0430\u0435\u0442\u0020\u043E\u0020\u0442\u043E\u043C\u0020\u0447\u0442\u043E\u0020\u043A\u043B\u0430\u0441\u0441\u0020\u043D\u0435\u0020\u0441\u043E\u043E\u0442\u0432\u0435\u0442\u0441\u0442\u0432\u0443\u0435\u0442\u0020\u0438\u043D\u0442\u0435\u0440\u0444\u0435\u0439\u0441\u0443\u0020\u0027LoggerInterface\u0027.",
+            "summary": "\u0418\u0441\u043A\u043B\u044E\u0447\u0435\u043D\u0438\u0435\u0020\u0441\u043E\u043E\u0431\u0449\u0430\u0435\u0442\u0020\u043E\u0020\u043D\u0435\u0432\u0435\u0440\u043D\u043E\u0439\u0020\u0433\u0435\u043D\u0435\u0440\u0430\u0446\u0438\u0438\u0020\u0441\u043E\u043E\u0431\u0449\u0435\u043D\u0438\u0439.",
             "url": "classes/vadimcontenthunter-MyLogger-exceptions-IncorrectMessageGenerationException.html"
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\IncorrectMessageGenerationException\u003A\u003A__construct\u0028\u0029",
@@ -343,7 +343,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\InvalidArgumentException",
             "name": "InvalidArgumentException",
-            "summary": "\u0418\u0441\u043A\u043B\u044E\u0447\u0435\u043D\u0438\u0435\u0020\u0441\u043E\u043E\u0431\u0449\u0430\u0435\u0442\u0020\u043E\u0020\u0442\u043E\u043C\u0020\u0447\u0442\u043E\u0020\u043A\u043B\u0430\u0441\u0441\u0020\u043D\u0435\u0020\u0441\u043E\u043E\u0442\u0432\u0435\u0442\u0441\u0442\u0432\u0443\u0435\u0442\u0020\u0438\u043D\u0442\u0435\u0440\u0444\u0435\u0439\u0441\u0443\u0020\u0027LoggerInterface\u0027.",
+            "summary": "\u0418\u0441\u043A\u043B\u044E\u0447\u0435\u043D\u0438\u0435\u0020\u0441\u043E\u043E\u0431\u0449\u0430\u0435\u0442\u0020\u043E\u0020\u043D\u0435\u043F\u0440\u0430\u0432\u0438\u043B\u044C\u043D\u043E\u043C\u0020\u0430\u0440\u0433\u0443\u043C\u0435\u043D\u0442\u0435\u0020\u043C\u0435\u0442\u043E\u0434\u0430.",
             "url": "classes/vadimcontenthunter-MyLogger-exceptions-InvalidArgumentException.html"
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\InvalidArgumentException\u003A\u003A__construct\u0028\u0029",
@@ -353,7 +353,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\NoFormatterException",
             "name": "NoFormatterException",
-            "summary": "\u0418\u0441\u043A\u043B\u044E\u0447\u0435\u043D\u0438\u0435\u0020\u0441\u043E\u043E\u0431\u0449\u0430\u0435\u0442\u0020\u043E\u0020\u0442\u043E\u043C\u0020\u0447\u0442\u043E\u0020\u043A\u043B\u0430\u0441\u0441\u0020\u043D\u0435\u0020\u0441\u043E\u043E\u0442\u0432\u0435\u0442\u0441\u0442\u0432\u0443\u0435\u0442\u0020\u0438\u043D\u0442\u0435\u0440\u0444\u0435\u0439\u0441\u0443\u0020\u0027LoggerInterface\u0027.",
+            "summary": "\u0418\u0441\u043A\u043B\u044E\u0447\u0435\u043D\u0438\u0435\u0020\u0441\u043E\u043E\u0431\u0449\u0430\u0435\u0442\u0020\u043E\u0020\u0442\u043E\u043C\u0020\u0447\u0442\u043E\u0020\u0444\u043E\u0440\u043C\u0430\u0442\u0435\u0440\u0020\u043A\u043B\u0430\u0441\u0441\u0020\u043D\u0435\u0020\u0441\u043E\u043E\u0442\u0432\u0435\u0442\u0441\u0442\u0432\u0443\u0435\u0442\u0020\u0438\u043D\u0442\u0435\u0440\u0444\u0435\u0439\u0441\u0443\u0020\u0027Formatter\u0027.",
             "url": "classes/vadimcontenthunter-MyLogger-exceptions-NoFormatterException.html"
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\NoFormatterException\u003A\u003A__construct\u0028\u0029",
@@ -368,7 +368,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\NotEnabledFlagException\u003A\u003A__construct\u0028\u0029",
             "name": "__construct",
-            "summary": "Initializes\u0020the\u0020SavingNotEnabledException",
+            "summary": "Initializes\u0020the\u0020NotEnabledFlagException",
             "url": "classes/vadimcontenthunter-MyLogger-exceptions-NotEnabledFlagException.html#method___construct"
         },                {
             "fqsen": "\\vadimcontenthunter\\MyLogger\\exceptions\\WrongExpectedResultException",
